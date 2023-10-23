8d0cbab299f292c71de344ddc87089e727794182
aulerok <aulerok@gmail.com>
2023-10-23 21:14:49 +0300

    test keyboards aiogram 3

---
+++ keyboards.py
@@ -0,0 +1,12 @@
+    #
+    # kb = [
+    #     [types.KeyboardButton(text="Купить новый курс"),
+    #      types.KeyboardButton(text="Восстановить доступ к курсу"),
+    #      types.KeyboardButton(text="Продлить доступ к курсу")
+    #      ],
+    # ]
+    # keyboard = types.ReplyKeyboardMarkup(
+    #     keyboard=kb,
+    #     resize_keyboard=True,
+    #     input_field_placeholder="Сделайте выбор"
+    # )
